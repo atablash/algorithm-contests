dedb78a0d765c82b3309b283574ccd6259149e50
Adam Blaszkiewicz <atablash@atablash.pl>
2018-07-09 04:00:53 +0200

    ...

--- template.cpp
+++ template.cpp
@@ -1,23 +1,26 @@
-#define USE_FAST_IO
+//#define READ_NUM_TEST_CASES
+//#define USE_FAST_IO
 #include "template.hpp"
 
 
 
 
 void test_case(int) {
-	int test;
-	cin >> test;
-	cout << "entered: " << test << endl;
+	
 }
 
 
 
 int main() {
-	int num_test_cases = ri();
+	#ifdef READ_NUM_TEST_CASES
+		int num_test_cases = ri();
 
-	for(int i=0; i < num_test_cases; ++i) {
-		test_case(i);
-	}
+		for(int i=0; i < num_test_cases; ++i) {
+			test_case(i);
+		}
+	#else
+		test_case(0);
+	#endif
 
 	return 0;
 }
